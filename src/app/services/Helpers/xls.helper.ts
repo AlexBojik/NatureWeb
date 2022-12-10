fcd7442876206ed0a5221210dfef73c9c05a1bc4
Derevenskikh Aleksander <hedgehog@kartis.ru>
2022-12-10 16:22:17 +0300

    fix-angular: add points array import

--- src/app/services/Helpers/xls.helper.ts
+++ src/app/services/Helpers/xls.helper.ts
@@ -1,56 +1,10 @@
 import { Dictionary, Value, DictionariesService } from './../dictionaries.service';
-import { CoordsHelper } from './coords.helper';
+import { CoordsHelper, PointLongLat } from './coords.helper';
 
 export class XLSRowData {
   fields: any = {}
-  // name: string
-  // coords: string
-  // coordsDec: string
-  // lat: string
-  // lon: string
-  // points: string
-  // constructor (row:any[], headerIndexes: XLSHeaderIndexes) {
-  //   this.name = headerIndexes.nameIndex == -1 ? null : row[headerIndexes.nameIndex]
-  //   this.coords = headerIndexes.coordIndex == -1 ? null : row[headerIndexes.coordIndex]
-  //   this.coordsDec = headerIndexes.coordDecIndex == -1 ? null : row[headerIndexes.coordDecIndex]
-  //   this.lat = headerIndexes.latIndex == -1 ? null : row[headerIndexes.latIndex]
-  //   this.lon = headerIndexes.lonIndex == -1 ? null : row[headerIndexes.lonIndex]
-  //   this.points = headerIndexes.pointsIndex == -1 ? null : row[headerIndexes.pointsIndex]
-  // }
 }
 
-class XLSHeaderIndexes {
-
-  nameIndex: number
-  coordIndex: number
-  coordDecIndex: number
-  latIndex: number
-  lonIndex: number
-  pointsIndex: number
-
-  constructor(headers: string[]) {
-    this.nameIndex = headers.findIndex ( h => h === 'Наименование')
-    this.coordIndex = headers.findIndex ( h => h === 'Координаты')
-    this.coordDecIndex = headers.findIndex ( h => h === 'Координаты (десятичные)')
-    this.latIndex = headers.findIndex ( h => h === 'СШ')
-    this.lonIndex = headers.findIndex ( h => h === 'ВД')
-    this.pointsIndex = headers.findIndex ( h => h === 'Точки')
-  }
-}
-
-class XLSColumn {
-  name: string;
-  length: number;
-}
-
-const ExportXLSColumns: XLSColumn[] = [
-  { name: 'Наименование', length: 1 },
-  { name: 'Координаты', length: 1 },
-  { name: 'Координаты (десятичные)', length: 2 },
-  { name: 'СШ', length: 3 },
-  { name: 'ВД', length: 3 },
-  { name: 'Точки', length: 1 },
-]
 
 export class XLSHelper {
 
@@ -82,6 +36,31 @@ export class XLSHelper {
   }
 
 
+  static objectsPointsParse(textString: any): any[] {
+    let result = [];
+    const regMask = /(?<text>.*) *(?<lat>..°.{1,2}΄.{1,10}")(.*)(?<lon>..°.{1,2}΄.{1,10}")/g
+    let latSH = 0;
+    let lonVD = 0;
+    let match;
+
+    do {
+      match = regMask.exec(textString);
+      if (match) {
+        let lon = XLSHelper.gmsToDeg(match.groups?.lon)
+        let lat = XLSHelper.gmsToDeg(match.groups?.lat)
+        let text = (match.groups?.text ?? '').trim()
+        result.push({text, lon, lat})
+        console.log(match.groups)
+      }
+    } while (match);
+    return result
+  }
+
+  static gmsToDeg(textGMS: any): number {
+    const [g, ms] = textGMS.split('°');
+    const [m, s] = ms.split('΄');
+    return parseInt(g, 0) + parseInt(m, 0) / 60 + parseFloat(s.replace('"', '')) / 3600;
+  }
 
   static async objectsForXLSRow(row: XLSRowData, dbFields: any[], dictSrv: DictionariesService): Promise<any[]> {
 
@@ -99,6 +78,11 @@ export class XLSHelper {
     return XLSHelper.makeFieldsData(dbFields, row, dictSrv).then( fieldsValue => {
       let result = []
       if (pointsValue && pointsValue.length > 0) {
+        let points = XLSHelper.objectsPointsParse(pointsValue)
+        for (const point of points) {
+          let obj = {coordinates: [[point.lon, point.lat]], fields: fieldsValue, name: nameValue + ' ' + point.text};
+          result.push(obj)
+        }
 
       } else if (coordValue && coordValue.length > 0) {
         let obj = {coordinates: [], fields: fieldsValue, name: nameValue};
@@ -152,200 +136,5 @@ export class XLSHelper {
 
     return Promise.resolve(result)
   }
-
 }
 
-
-
-    // let currentHead = headers[0];
-    // let currentField = allFields.find(f => f.name === currentHead);
-    // let currentValue = [];
-    // let currentObj = {coordinates: [], fields: [], name};
-
-    // for (const str of rows) {
-    //   let lon = 0.0;
-    //   let lat = 0.0;
-    //   let k = 1.0;
-    //   if (str[0] !== undefined) {
-    //     if (currentObj.coordinates.length > 0) {
-    //       if (currentValue.length > 0) {
-    //         currentObj.fields.push({fieldId: currentField.id, value: currentValue.join(' ')});
-    //         currentValue = [];
-    //       }
-    //       objects.push(currentObj);
-    //     }
-    //     currentObj = {coordinates: [], fields: [], name};
-    //   }
-    //   for (let i = 0; i < len; i++) {
-    //     if (headers[i] !== undefined) {
-    //       if (currentValue.length > 0) {
-    //         currentObj.fields.push({fieldId: currentField.id, value: currentValue.join(' ')});
-    //         currentValue = [];
-    //       }
-    //       currentHead = headers[i];
-    //       currentField = allFields.find(f => f.name === currentHead);
-    //     }
-    //     if (currentHead === 'СШ') {
-    //       lat += (str[i] / k);
-    //       if (k === 3600.0) {
-    //         k = 1.0;
-    //       } else {
-    //         k *= 60.0;
-    //       }
-    //     } else if (currentHead === 'ВД') {
-    //       lon += str[i] / k;
-    //       if (k === 3600.0) {
-    //         k = 1.0;
-    //       } else {
-    //         k *= 60.0;
-    //       }
-    //     }
-    //     if (!str[i]) {
-    //       continue;
-    //     }
-
-    //     if (currentHead === 'Наименование') {
-    //       currentObj.name = str[i]?.trim();
-    //     } else if (currentHead === 'Координаты') {
-    //       let coords = CoordsHelper.parseGMSFromString(str[i])
-
-    //       coords.forEach(coordPair => {
-    //         // Note: Mapbox GL uses longitude, latitude coordinate order (as opposed to latitude, longitude) to match GeoJSON.
-    //         currentObj.coordinates.push([coordPair.longitude, coordPair.latitude])
-    //       });
-
-    //     } else if (currentHead === 'Координаты (десятичные)') {
-    //       lat = parseFloat(str[i]);
-    //       lon = parseFloat(str[++i]);
-    //     } else {
-    //       if (!!currentField) {
-    //         if (!str[i]) {
-    //           continue;
-    //         }
-    //         if (currentField.type > 0) {
-    //           let opt = currentField.options.find(o => o.name === str[i]);
-    //           if (!opt) {
-    //             const value = new Value();
-    //             value.name = str[i];
-    //             value.dictId = currentField.type;
-    //             await this._dictSrv.saveValue(value).then(id => {
-    //               opt = {id, name: str[i]};
-    //               currentField.options.push(opt);
-    //             });
-    //           }
-    //           currentObj.fields.push({fieldId: currentField.id, valueNum: opt.id});
-    //         } else {
-    //           currentValue.push(str[i]);
-    //         }
-    //       }
-    //     }
-    //   }
-    //   if (currentValue.length > 0) {
-    //     currentObj.fields.push({fieldId: currentField.id, value: currentValue.join(' ')});
-    //     currentValue = [];
-    //   }
-    //   if (lon > 0 && lat > 0 || currentObj.coordinates.length === 0) {
-    //     currentObj.coordinates.push([lon, lat]);
-    //   }
-    // }
-    // objects.push(currentObj);
-
-
-
-    // function parseXLSrow(row: any, headers: any): object[] {
-    //   // for (const str of rows) {
-    //   //   let lon = 0.0;
-    //   //   let lat = 0.0;
-    //   //   let k = 1.0;
-    //   //   if (str[0] !== undefined) {
-    //   //     if (currentObj.coordinates.length > 0) {
-    //   //       if (currentValue.length > 0) {
-    //   //         currentObj.fields.push({fieldId: currentField.id, value: currentValue.join(' ')});
-    //   //         currentValue = [];
-    //   //       }
-    //   //       objects.push(currentObj);
-    //   //     }
-    //   //     currentObj = {coordinates: [], fields: [], name};
-    //   //   }
-
-    //   let nameIndex = headers.find
-    //   for (let i = 0; i < headers.len; i++) {
-
-    //   }
-    //   let object = {}
-    //     for (let i = 0; i < len; i++) {
-    //       if (headers[i] !== undefined) {
-    //         if (currentValue.length > 0) {
-    //           currentObj.fields.push({fieldId: currentField.id, value: currentValue.join(' ')});
-    //           currentValue = [];
-    //         }
-    //         currentHead = headers[i];
-    //         currentField = allFields.find(f => f.name === currentHead);
-    //       }
-    //       if (currentHead === 'СШ') {
-    //         lat += (str[i] / k);
-    //         if (k === 3600.0) {
-    //           k = 1.0;
-    //         } else {
-    //           k *= 60.0;
-    //         }
-    //       } else if (currentHead === 'ВД') {
-    //         lon += str[i] / k;
-    //         if (k === 3600.0) {
-    //           k = 1.0;
-    //         } else {
-    //           k *= 60.0;
-    //         }
-    //       }
-    //       if (!str[i]) {
-    //         continue;
-    //       }
-
-    //       if (currentHead === 'Наименование') {
-    //         currentObj.name = str[i]?.trim();
-    //       } else if (currentHead === 'Координаты') {
-    //         let coords = CoordsHelper.parseGMSFromString(str[i])
-
-    //         coords.forEach(coordPair => {
-    //           // Note: Mapbox GL uses longitude, latitude coordinate order (as opposed to latitude, longitude) to match GeoJSON.
-    //           currentObj.coordinates.push([coordPair.longitude, coordPair.latitude])
-    //         });
-
-    //       } else if (currentHead === 'Координаты (десятичные)') {
-    //         lat = parseFloat(str[i]);
-    //         lon = parseFloat(str[++i]);
-    //       } else {
-    //         if (!!currentField) {
-    //           if (!str[i]) {
-    //             continue;
-    //           }
-    //           if (currentField.type > 0) {
-    //             let opt = currentField.options.find(o => o.name === str[i]);
-    //             if (!opt) {
-    //               const value = new Value();
-    //               value.name = str[i];
-    //               value.dictId = currentField.type;
-    //               await this._dictSrv.saveValue(value).then(id => {
-    //                 opt = {id, name: str[i]};
-    //                 currentField.options.push(opt);
-    //               });
-    //             }
-    //             currentObj.fields.push({fieldId: currentField.id, valueNum: opt.id});
-    //           } else {
-    //             currentValue.push(str[i]);
-    //           }
-    //         }
-    //       }
-    //     }
-    //     if (currentValue.length > 0) {
-    //       currentObj.fields.push({fieldId: currentField.id, value: currentValue.join(' ')});
-    //       currentValue = [];
-    //     }
-    //     if (lon > 0 && lat > 0 || currentObj.coordinates.length === 0) {
-    //       currentObj.coordinates.push([lon, lat]);
-    //     }
-    //   }
-    //   objects.push(currentObj);
-
-    //   return []
-    // }
