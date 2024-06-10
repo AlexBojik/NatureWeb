3f3d90a4ceae5d7c0759a4a7193aa531056ef7fb
Derevenskikh Aleksander <hedgehog@kartis.ru>
2024-06-10 11:05:10 +0300

    fix-angular: add count for selected objects [redmine-15779]

--- src/app/objects/object-table/object-table.component.ts
+++ src/app/objects/object-table/object-table.component.ts
@@ -1,4 +1,4 @@
-import { ProgressDialogComponent } from './../../progress-dialog/progress-dialog.component';
+import {ProgressDialogComponent} from './../../progress-dialog/progress-dialog.component';
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {Layer, LayersService} from '../../layers/layers.service';
 import {MatTableDataSource} from '@angular/material/table';
@@ -11,8 +11,8 @@ import {MapboxComponent} from '../../mapbox/mapbox.component';
 import * as mapboxgl from 'mapbox-gl';
 import {MapService} from '../../services/map.service';
 import {NavigateService} from '../../services/navigate.service';
-import { MatSort } from '@angular/material/sort';
-import { Console } from 'console';
+import {MatSort} from '@angular/material/sort';
+import {Console} from 'console';
 
 @Component({
   selector: 'app-object-table',
@@ -30,23 +30,18 @@ export class ObjectTableComponent implements OnInit {
   current: GeoObject;
   layerLoaded: Layer;
 
-  _checkedAll: boolean
-  public set checkedAll(v: boolean) {
-    this._checkedAll = v;
+  _isCheckedAll: boolean;
+  checkedCount: number;
+
+  public set isCheckedAll(value: boolean) {
+    this._isCheckedAll = value;
     for (const obj of this.objects) {
-      obj.checked = this._checkedAll
+      obj.checked = this._isCheckedAll;
     }
   }
 
-  public get checkedAll() : boolean {
-    return this._checkedAll
-  }
-
-
-
-  applyFilter(event: Event): void {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  public get isCheckedAll(): boolean {
+    return this._isCheckedAll;
   }
 
   constructor(private _layersSrv: LayersService,
@@ -54,22 +49,39 @@ export class ObjectTableComponent implements OnInit {
               private _snackBar: MatSnackBar,
               private _mapSrv: MapService,
               private _navSrv: NavigateService,
-              public dialog: MatDialog) {
+              public dialog: MatDialog
+  ) {
     this._layersSrv.selected$.subscribe(layer => {
-      this.update(layer)
+      this.update(layer);
     });
   }
-  update(layer: Layer) {
 
-    if (this.layerLoaded?.id === layer.id) { return }
-    this.layerLoaded = layer
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.objects.forEach(obj  =>  {
+      obj.checked = this.dataSource.filteredData.find(obj1 => (obj1 === obj) && obj.checked) === obj;
+    });
+    this.recalcCheckedCount();
+  }
+
+  update(layer: Layer): void {
+
+    if (this.layerLoaded?.id === layer.id) {
+      return;
+    }
+    this.layerLoaded = layer;
     this.isLoading = true;
 
     this._objSrv.getObjects(layer.id)
       .then(objects => {
 
-        if (!layer){ return  }
-        if (layer.id !== this.layerLoaded.id) { return }
+        if (!layer) {
+          return;
+        }
+        if (layer.id !== this.layerLoaded.id) {
+          return;
+        }
         this.dataSource = null;
 
         this.objects = [];
@@ -82,6 +94,7 @@ export class ObjectTableComponent implements OnInit {
 
         this.dataSource = new MatTableDataSource<GeoObject>(this.objects);
         this.dataSource.sort = this.empTbSort;
+        this.recalcCheckedCount();
         this.isLoading = false;
       });
 
@@ -107,37 +120,38 @@ export class ObjectTableComponent implements OnInit {
   delete(obj: GeoObject): void {
     // TODO: вопрос об удалении
     this.dialog.open(ProgressDialogComponent);
-    let deleteList = this.objects.filter( o => o.checked)
+    let deleteList = this.objects.filter(o => o.checked);
     if (deleteList.length === 0) {
-      obj.checked = true
-      deleteList = [obj]
+      obj.checked = true;
+      deleteList = [obj];
     }
 
-    const deletePromises = deleteList.map (deleteObj => {
+    const deletePromises = deleteList.map(deleteObj => {
       return this._objSrv.deleteObject(deleteObj.id)
         .then(() => {
-          return {id: deleteObj.id, success: true}
+          return {id: deleteObj.id, success: true};
         }).catch(() => {
-          return {id: deleteObj.id, success: false}
-      });
+          return {id: deleteObj.id, success: false};
+        });
     });
 
-    Promise.all(deletePromises).then( (results) => {
-      const isHasError = results.find( r => !r.success) || false
+    Promise.all(deletePromises).then((results) => {
+      const isHasError = results.find(r => !r.success) || false;
       if (isHasError) {
         this._snackBar.open('Не удалось удалить минимум один элемент!', 'OK', {duration: 500});
       }
-      const successDeletedIds = results.filter( r => r.success).map ( r => r.id)
+      const successDeletedIds = results.filter(r => r.success).map(r => r.id);
       for (const objectId of successDeletedIds) {
-        const deletedObjIndex = this.objects.findIndex( o => o.id == objectId)
+        const deletedObjIndex = this.objects.findIndex(o => o.id == objectId);
         if (deletedObjIndex !== undefined) {
           this.objects.splice(deletedObjIndex, 1);
         }
       }
       this.dataSource = new MatTableDataSource<GeoObject>(this.objects);
-      this._checkedAll = false
-      this.dialog.closeAll()
-    })
+      this._isCheckedAll = false;
+      this.dialog.closeAll();
+      this.recalcCheckedCount();
+    });
 
     // this._objSrv.deleteObject(obj.id)
     //   .then(() => {
@@ -152,6 +166,10 @@ export class ObjectTableComponent implements OnInit {
 
   edit(obj: GeoObject): void {
 
+  }
+
+  checkClick(element): void {
+    this.recalcCheckedCount();
   }
 
   pick(obj: GeoObject): void {
@@ -175,5 +193,12 @@ export class ObjectTableComponent implements OnInit {
       this._mapSrv.map.fitBounds(bounds, {padding: 100});
     }
     this._navSrv.showMap();
+    this.recalcCheckedCount();
   }
+
+  private recalcCheckedCount(): void {
+    this._isCheckedAll = this.objects.every(o => o.checked);
+    this.checkedCount = this.objects.filter(o => o.checked).length;
+  }
+
 }
